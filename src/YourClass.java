e8f9aea94887e0c5f9df311cf24c71c59f4c855b
jessehartloff <hartloff@buffalo.edu>
2017-03-14 15:09:09 -0400

    initial commit

---
+++ src/YourClass.java
@@ -0,0 +1,29 @@
+
+public class YourClass{
+
+
+    // 1 point
+    // Create 2 ints as instance variables named 'a' and 'b'
+
+
+
+    // 1 point
+    // Write a constructor that takes 2 ints as parameters and sets the values of the parameters to the instance
+    // variables. The first parameter is stored in 'a' and the second parameter is stored in 'b'
+
+
+
+    // 1 points
+    // Write getter methods for 'a' and 'b' named "getA" and "getB"
+
+
+
+    // 1 points
+    // Write setter methods for 'a' and 'b' named "setA" and "setB"
+
+
+
+    // 1 point
+    // Write a method named difference that takes no parameters and returns the subtraction of 'a' minus 'b' as an int
+
+}
